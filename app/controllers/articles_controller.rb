b918696d66537ba45715465dc9ea3ff6cef67956
Isil Donmez <isil.donmez@gmail.com>
2018-03-06 15:33:36 +0000

    Add Delete action; Change show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,5 +20,11 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path(@article)
+  end
 
 end
