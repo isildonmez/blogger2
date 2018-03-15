4e71eb2d5a8d7533639c8ed0cd447785e08d9834
Isil Donmez <isil.donmez@gmail.com>
2018-03-15 17:20:37 +0000

    Finish Authentication; Blogger Project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
 
+  before_action :require_login, only: [:index, :show]
+
   def index
     @articles = Article.all
   end
